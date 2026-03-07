1102bdce02ba7d6016e611223b7125099387e5d7
kamyloch <camilochto@gmail.com>
2026-03-07 15:04:56 +0100

    Comentado reserva string

--- Practica1/src/prog2/model/Reserva.java
+++ Practica1/src/prog2/model/Reserva.java
@@ -50,10 +50,10 @@ public class Reserva implements InReserva {
         return ChronoUnit.DAYS.between(dataEntrada,dataSortida);
     }
 
-    //Mètodes
+    //Mètodes(No demanat)/*
     public String toString (){
         return "Client: " + client.getNom() + " ("+ client.getDni()+")\n"+
                 "Allotjament: " + allotjament.getNom() + " ("+ allotjament.getId()+")\n"+
                 "Estada: " + dataEntrada.toString() + " a " +dataSortida.toString() + " ("+ getEstada()+" dies)";
-    }
+    }*/
 }
