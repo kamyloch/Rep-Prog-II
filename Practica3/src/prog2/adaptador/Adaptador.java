ef4d775fb7aa53ad8c5838b09870aefdbc8b1d35
janpi14 <janivetjanivet@gmail.com>
2026-04-22 20:59:20 +0200

    adaprador

--- Practica3/src/prog2/adaptador/Adaptador.java
+++ Practica3/src/prog2/adaptador/Adaptador.java
@@ -1,4 +1,82 @@
 package prog2.adaptador;
 
+import prog2.model.Dades;
+import prog2.vista.BiblioException;
+
+import java.io.*;
+
 public class Adaptador {
+    private Dades dades;
+
+    public Adaptador(){
+        dades=new Dades();
+    }
+
+
+
+
+
+
+    public void guardaDades(String camiDesti) throws BiblioException{
+        File fitxer = new File(camiDesti);
+
+        FileOutputStream fout = null;
+        ObjectOutputStream oos = null;
+        try{
+            fout=new FileOutputStream(fitxer);
+        }catch(FileNotFoundException ex){
+            throw new BiblioException("El fitxer no s'ha trobat");
+        }
+
+        try{
+            oos=new ObjectOutputStream(fout);}
+        catch(IOException ex){
+            throw new BiblioException ("No s'ha pogut crear l'objecte per escriure al fitxer");
+        }try {
+            oos.writeObject(this);
+        }catch(IOException ex){
+            throw new BiblioException("No s'ha pogut escriure al fitxer");
+        }finally {
+            try {
+                oos.close();
+                fout.close();
+            } catch (IOException ex) {
+                throw new BiblioException("No s'ha pogut tancar el fitxer");
+            }
+        }
+    }
+
+    public void carregaDades(String camiDesti) throws BiblioException{
+        File fitxer = new File(camiDesti);
+        FileInputStream fin = null;
+        ObjectInputStream ois = null;
+        try{
+            fin=new FileInputStream(fitxer);
+        }catch(FileNotFoundException ex){
+            throw new BiblioException("El fitxer no s'ha trobat");
+        }
+
+        try{
+            ois=new ObjectInputStream(fin);}
+        catch(IOException ex){
+            throw new BiblioException ("No s'ha pogut crear l'objecte per llegir al fitxer");
+        }try {
+            dades= (Dades) ois.readObject();
+        }catch(IOException | ClassNotFoundException ex){
+            throw new BiblioException("No s'ha pogut llegir al fitxer");
+        }finally {
+            try {
+                ois.close();
+                fin.close();
+            } catch (IOException ex) {
+                throw new BiblioException("No s'ha pogut tancar el fitxer");
+            }
+        }
+
+
+
+
+
+
+
 }
